39e1372b4291a20a5523b8e5d2cbec54aa19a564
Samuel Cornet <samuel@noukod.com>
2019-01-21 16:27:13 +0000

    Automatically backed up by Learn

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,33 @@
+class SchoolClassesController < ApplicationController
+
+  def new
+    @school_class = SchoolClass.new
+  end
+
+  def show
+    set_schoolclass
+  end
+
+  def create
+    @school_class = SchoolClass.create post_params(:title, :room_number)
+		redirect_to post_path(@school_class)
+  end
+
+  def edit
+    set_schoolclass
+  end
+
+  def update
+    set_schoolclass
+    @school_class.update post_params(:title)
+  end
+
+  def set_schoolclass
+    @school_class = SchoolClass.find(params[:id])
+  end
+    
+
+
+
+end
+
---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,23 @@
+class StudentsController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def show
+
+  end
+
+  def update
+
+  end
+
+end
